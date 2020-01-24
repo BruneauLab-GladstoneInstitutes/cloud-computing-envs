f0b96d41bd2557f4d63cb804d0406c7860f44bc5
Andrew Blair <andrewblair@LAXUCD07855.local>
2020-01-23 17:22:16 -0800

    updated information in conf

---
+++ atac-seq.sh
@@ -0,0 +1,11 @@
+# qb3-dev1 node
+git clone https://github.com/ENCODE-DCC/atac-seq-pipeline.git
+python3 -m pip install --user caper
+
+cd ~/.caper
+# Add sge to smp and specify output dir in default.con
+
+cd atac-seq-pipeline/
+
+bash scripts/download_genome_data.sh hg38 ../
+echo "caper run /wynton/home/bruneau/ablair/atac-seq-pipeline/atac.wdl -i test.json" | qsub -V -N atac-test -pe smp 4 -l mem_free=6G -l h_rt=02:00:00 
